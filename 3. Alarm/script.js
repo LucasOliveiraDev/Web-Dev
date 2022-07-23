0c3203d59d4341e39cd53e38896cd489a996ea79
LucasOliveiraDev <lucas.oliver.silva@hotmail.com>
2022-07-22 21:49:47 -0300

    Alarm updated.

---
+++ 3. Alarm/script.js
@@ -0,0 +1,27 @@
+/* PEGANDO HORAS
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();*/
+
+const display = document.getElementById('clock');
+
+function updateTime(){
+    const date = new Date();
+
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
+
+    display.innerText=`${hour}:${minutes}:${seconds}`;
+
+}
+
+function formatTime(time){
+    if(time<10){
+        return '0'+time;
+    }
+
+    return time;
+}
+
+setInterval(updateTime, 1000)
